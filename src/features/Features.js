5129a27f9d2631ca91b8ba51e0badc6608423789
jcolejeff <ikwuhj@gmail.com>
2022-03-06 17:30:42 +0100

    changed css

--- src/features/Features.js
+++ src/features/Features.js
@@ -6,7 +6,7 @@ import img2 from "../images/illustration-features-tab-2.svg";
 import img3 from "../images/illustration-features-tab-3.svg";
 function App() {
   const [value, setValue] = useState(0);
-  const jobs = [
+  const projects = [
     {
       id: "recIL6mJNfWObonls",
       title: "Bookmark in one click",
@@ -30,7 +30,7 @@ function App() {
     },
   ];
 
-  const { info, title, image } = jobs[value];
+  const { info, title, image } = projects[value];
   return (
     <Div>
       <section className="section">
@@ -44,24 +44,24 @@ function App() {
           </p>
           <div className="underline"></div>
         </div>
-        <div className="jobs-center">
+        <div className="projects-center">
           {/* btn container */}
           <div className="btn-container">
-            {jobs.map((item, index) => {
+            {projects.map((item, index) => {
               return (
                 <button
                   key={item.id}
                   onClick={() => setValue(index)}
-                  className={`job-btn ${index === value && "active-btn"}`}
+                  className={`project-btn ${index === value && "active-btn"}`}
                 >
                   {item.button}
                 </button>
               );
             })}
           </div>
-          {/* job info */}
-          <article className="job-info">
-            <div className="job-desc">
+          {/* project info */}
+          <article className="project-info">
+            <div className="project-desc">
               <h3>{title}</h3>
               <p>{info}</p>
             </div>
@@ -80,13 +80,12 @@ const Div = styled.div`
   margin-block-start: 4rem;
   padding: 2rem;
   box-shadow: var(--dark-shadow);
-  .underline {
-    width: 5rem;
-    height: 0.25rem;
-    margin-bottom: 1.25rem;
-    background: var(--clr-primary-5);
-    margin-left: auto;
-    margin-right: auto;
+
+  figure {
+    width: 100%;
+    img {
+      width: 100%;
+    }
   }
   .title {
     margin-bottom: 4rem;
@@ -95,118 +94,5 @@ const Div = styled.div`
       text-align: center;
       padding: 0rem 5rem 0rem 5rem;
     }
-  }
-
-  figure {
-    width: 100%;
-    img {
-      width: 100%;
-    }
-  }
-
-  /*
-=============== 
-Jobs
-===============
-*/
-
-  .jobs-center {
-    width: 80vw;
-    margin: 0 auto;
-    max-width: var(--max-width);
-  }
-  .btn-container {
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-    margin-bottom: 4rem;
-    flex-wrap: wrap;
-  }
-  .job-btn {
-    background: transparent;
-    border-color: transparent;
-    text-transform: capitalize;
-    font-size: 1.25rem;
-    letter-spacing: var(--spacing);
-    margin: 0 0.5rem;
-    transition: var(--transition);
-    cursor: pointer;
-    padding: 0.25rem 0;
-    line-height: 1;
-    outline-color: var(--clr-primary-10);
-  }
-  .job-btn:hover {
-    color: var(--clr-primary-5);
-    box-shadow: 0 2px var(--clr-primary-5);
-  }
-  .active-btn {
-    color: var(--clr-primary-5);
-    box-shadow: 0 2px var(--clr-primary-5);
-  }
-
-  .job-info h3 {
-    font-weight: 400;
-  }
-  .job-info h4 {
-    text-transform: uppercase;
-    color: var(--clr-grey-5);
-    background: var(--clr-grey-9);
-    display: inline-block;
-    padding: 0.375rem 0.75rem;
-    border-radius: var(--radius);
-  }
-  .job-date {
-    letter-spacing: var(--spacing);
-  }
-  .job-desc {
-    margin-bottom: 1.25rem;
-    text-align: center;
-  }
-  .job-desc p {
-    margin-bottom: 0;
-    color: var(--clr-grey-3);
-  }
-  .job-icon {
-    color: var(--clr-primary-5);
-  }
-  @media screen and (min-width: 992px) {
-    .job-info {
-      display: flex;
-    }
-    .job-btn {
-      margin-bottom: 1rem;
-    }
-    .active-btn {
-      box-shadow: -2px 0 var(--clr-primary-5);
-    }
-    .job-btn:hover {
-      box-shadow: -2px 0 var(--clr-primary-5);
-    }
-  }
-  .btn {
-    text-transform: uppercase;
-    background: var(--clr-primary-5);
-    color: var(--clr-primary-9);
-    padding: 0.375rem 0.75rem;
-    letter-spacing: var(--spacing);
-    font-weight: 700;
-    -webkit-transition: var(--transition);
-    transition: var(--transition);
-    font-size: 0.875rem;
-    border: 2px solid transparent;
-    cursor: pointer;
-    box-shadow: 0 1px 3px rgba(0, 0, 0, 0.2);
-    border-radius: var(--radius);
-    display: block;
-    width: 12rem;
-    text-align: center;
-    margin: 0 auto;
-    margin-top: 3rem;
-  }
-  .btn:hover {
-    color: var(--clr-primary-1);
-    background: var(--clr-primary-8);
-  }
-  .center-btn {
   }
 `;
